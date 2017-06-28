76b80c227a1409c4670311d17e35437cfdae9bb0
Alex M <alex.m.ysbm@gmail.com>
2017-06-28 09:23:52 +0300

    solution

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ window.onload = function() {
     }
 };
 
-function injectTheScript() {
+function injectTheScript(e) {
+    e.preventDefault();
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     var params = "lorem=ipsum&name=binny";
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 201) {
             console.log(this.responseText);
         }
 
